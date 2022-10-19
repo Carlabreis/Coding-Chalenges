43cc9c40fb45861f685f1a434aeb0d4b365de667
Carla Reis <carlareis.dev@gmail.com>
2022-10-19 18:23:12 -0400

    update with PREP w/ Leon for Huntober standup

--- getTheMeanOfAnArray.js
+++ getTheMeanOfAnArray.js
@@ -5,3 +5,24 @@
 function getAverage(marks){
   return Math.floor(marks.reduce((a,b) => a + b) / marks.length)
 }
+
+
+// SOLUTION W/ HUNTOBER STAND UP 10.19.22 W/ LEON
+
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+function getAverage(arr){
+  //loop thru arr to get sum
+  return Math.floor(arr.reduce((a,c) => a + c) / arr.length)
+  //avg -> sum / arr.lenght
+  // return
+}
+// const getAverage = arr => Math.floor(arr.reduce((a, c) => a + c) / arr.length)
+
+console.log(getAverage([2, 2, 2, 2]), 2);
+console.log(getAverage([1, 2, 3, 4, 5,]), 3);
+console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]), 1)
