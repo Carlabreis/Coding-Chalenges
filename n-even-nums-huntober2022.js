a8b642eb4f69ad14803c93e6ba5baae3726634dc
Carla Reis <carlareis.dev@gmail.com>
2023-03-09 21:42:46 -0400

    coding challenge for huntober

---
+++ n-even-nums-huntober2022.js
@@ -0,0 +1,28 @@
+// Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+// For example:
+
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+//[arr], num
+//never empty, at least num in the array 
+//[2,4], 2
+//[] -> last even nums 
+
+// function nEvenNums(arr,num){
+//   //filter out odd nums
+//   //n last nums from the filtered array - slice (-n)
+//   return arr.filter(n => n % 2 === 0).slice(-num)
+// }
+
+const nEvenNums = (arr, num) => {
+    const even = arr.filter(n => n%2 === 0).slice(-num)
+  }
+  
+  // Leon's result:
+  // const nEvenNums = (arr, num) => arr.filter(n => n % 2 === 0).slice(-num)
+  
+  console.log(nEvenNums([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), ' [4, 6, 8]')
+  console.log(nEvenNums([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2), '[-8, 26]')
